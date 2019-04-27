33d6559f82e3366bc0a9ebe8be64e76401769f30
49545 <495454126@qq.com>
2019-04-27 23:11:36 +0800

    1

--- app/src/main/java/com/example/lan/testproject/MainActivity.java
+++ app/src/main/java/com/example/lan/testproject/MainActivity.java
@@ -56,6 +56,8 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         });
+        //app:cardElevation="0px"
+        //app:cardUseCompatPadding="false"
     }
 
 
